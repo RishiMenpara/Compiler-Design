4dbb53143649513bfaec818a9c3430cdd2dc0bf9
Rishi Menpara <reesheemenpara1@gmail.com>
2025-09-24 23:08:44 +0530

    final main.cpp

--- main.cpp
+++ main.cpp
@@ -1,195 +1,256 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
 using namespace std;
 
-enum class TokenType {
-    number, plus, minus, mul, div, lparen, rparen, eof
+enum class TokenType
+{
+    NUMBER,
+    PLUS,
+    MINUS,
+    MUL,
+    DIV,
+    LPAREN,
+    RPAREN,
+    END_OF_FILE
 };
 
-struct Token {
+struct Token
+{
     TokenType type;
-    string text;
+    string value;
 };
 
-class Lexer {
+class Lexer
+{
 private:
-    string input;
-    int index;
-    
+    string src;
+    int pos;
+
 public:
-    Lexer(const string& source) : input(source), index(0) {}
+    Lexer(const string &source) : src(source), pos(0) {}
 
-    Token getNext() {
-        while (index < input.size()) {
-            char ch = input[index];
-            
-            if (isspace(ch)) { 
-                index++; 
-                continue; 
+    Token getNextToken()
+    {
+        while (pos < (int)src.size())
+        {
+            char current = src[pos];
+            if (isspace(current))
+            {
+                pos++;
+                continue;
             }
 
-            if (isdigit(ch)) {
+            if (isdigit(current))
+            {
                 string num;
-                while (index < (int)input.size() && isdigit(input[index])) {
-                    num.push_back(input[index++]);
-                }
-                return {TokenType::number, num};
+                while (pos < (int)src.size() && isdigit(src[pos]))
+                    num.push_back(src[pos++]);
+                return {TokenType::NUMBER, num};
             }
 
-            if (ch == '+') { index++; return {TokenType::plus, "+"}; }
-            if (ch == '-') { index++; return {TokenType::minus, "-"}; }
-            if (ch == '*') { index++; return {TokenType::mul, "*"}; }
-            if (ch == '/') { index++; return {TokenType::div, "/"}; }
-            if (ch == '(') { index++; return {TokenType::lparen, "("}; }
-            if (ch == ')') { index++; return {TokenType::rparen, ")"}; }
-
-            cerr << "Bad character: " << ch << endl;
-            index++;
+            pos++;
+            switch (current)
+            {
+            case '+':
+                return {TokenType::PLUS, "+"};
+            case '-':
+                return {TokenType::MINUS, "-"};
+            case '*':
+                return {TokenType::MUL, "*"};
+            case '/':
+                return {TokenType::DIV, "/"};
+            case '(':
+                return {TokenType::LPAREN, "("};
+            case ')':
+                return {TokenType::RPAREN, ")"};
+            default:
+                cerr << "Unexpected character: " << current << endl;
+            }
         }
-        return {TokenType::eof, ""};
+        return {TokenType::END_OF_FILE, ""};
     }
 };
 
-enum class NodeType { number, binop };
+enum class NodeType
+{
+    NUMBER,
+    BINOP
+};
 
-struct TreeNode {
+class BaseNode
+{
+protected:
     NodeType nodetype;
-    int data;
-    TreeNode* left;
-    Token op;
-    TreeNode* right;
 
-    TreeNode(int val) : nodetype(NodeType::number), data(val), left(nullptr), right(nullptr) {}
-    TreeNode(TreeNode* l, Token oper, TreeNode* r) : nodetype(NodeType::binop), data(0), left(l), op(oper), right(r) {}
-    
-    ~TreeNode() {
+public:
+    BaseNode(NodeType type) : nodetype(type) {}
+    inline NodeType getType() const { return nodetype; }
+    friend class Interpreter;
+};
+
+class NumNode : public BaseNode
+{
+    int value;
+
+public:
+    NumNode(int v) : BaseNode(NodeType::NUMBER), value(v) {}
+    inline int getValue() const { return value; }
+    friend class Interpreter;
+};
+
+class BinOpNode : public BaseNode
+{
+    BaseNode *left;
+    Token op;
+    BaseNode *right;
+
+public:
+    BinOpNode(BaseNode *l, Token o, BaseNode *r) : BaseNode(NodeType::BINOP), left(l), op(o), right(r) {}
+    inline BaseNode *getLeft() const { return left; }
+    inline BaseNode *getRight() const { return right; }
+    inline Token getOp() const { return op; }
+    ~BinOpNode()
+    {
         delete left;
         delete right;
     }
+    friend class Interpreter;
 };
 
-class Parser {
+class Parser
+{
 private:
-    Lexer &lex;
+    Lexer &lexer;
     Token token;
 
-    void eat(TokenType expected) {
-        if (token.type == expected) {
-            token = lex.getNext();
-        }
+    void eat(TokenType expected)
+    {
+        if (token.type == expected)
+            token = lexer.getNextToken();
     }
 
-    TreeNode* factor() {
-        if (token.type == TokenType::number) {
-            int val = stoi(token.text);
-            eat(TokenType::number);
-            return new TreeNode(val);
+    BaseNode *factor()
+    {
+        if (token.type == TokenType::NUMBER)
+        {
+            int val = stoi(token.value);
+            eat(TokenType::NUMBER);
+            return new NumNode(val);
         }
-        else if (token.type == TokenType::lparen) {
-            eat(TokenType::lparen);
-            TreeNode* result = expr();
-            eat(TokenType::rparen);
-            return result;
+        else if (token.type == TokenType::LPAREN)
+        {
+            eat(TokenType::LPAREN);
+            BaseNode *node = expr();
+            eat(TokenType::RPAREN);
+            return node;
         }
         return nullptr;
     }
 
-    TreeNode* term() {
-        TreeNode* result = factor();
-        
-        while (token.type == TokenType::mul || token.type == TokenType::div) {
+    BaseNode *term()
+    {
+        BaseNode *node = factor();
+        while (token.type == TokenType::MUL || token.type == TokenType::DIV)
+        {
             Token oper = token;
-            if (oper.type == TokenType::mul) 
-                eat(TokenType::mul);
-            else 
-                eat(TokenType::div);
-            result = new TreeNode(result, oper, factor());
+            if (oper.type == TokenType::MUL)
+                eat(TokenType::MUL);
+            else
+                eat(TokenType::DIV);
+            node = new BinOpNode(node, oper, factor());
         }
-        return result;
+        return node;
     }
 
-    TreeNode* expr() {
-        TreeNode* result = term();
-        
-        while (token.type == TokenType::plus || token.type == TokenType::minus) {
+    BaseNode *expr()
+    {
+        BaseNode *node = term();
+        while (token.type == TokenType::PLUS || token.type == TokenType::MINUS)
+        {
             Token oper = token;
-            if (oper.type == TokenType::plus) 
-                eat(TokenType::plus);
-            else 
-                eat(TokenType::minus);
-            result = new TreeNode(result, oper, term());
+            if (oper.type == TokenType::PLUS)
+                eat(TokenType::PLUS);
+            else
+                eat(TokenType::MINUS);
+            node = new BinOpNode(node, oper, term());
         }
-        return result;
+        return node;
     }
 
 public:
-    Parser(Lexer &lexer) : lex(lexer) {
-        token = lex.getNext();
-    }
-
-    TreeNode* parse() {
-        return expr();
-    }
+    Parser(Lexer &lex) : lexer(lex) { token = lexer.getNextToken(); }
+    BaseNode *parse() { return expr(); }
 };
 
-class Value {
+class Value
+{
 public:
-    int num;
-    
-    Value(int n = 0) : num(n) {}
-
-    Value operator+(const Value& other) const { 
-        return Value(num + other.num); 
-    }
-    Value operator-(const Value& other) const { 
-        return Value(num - other.num); 
-    }
-    Value operator*(const Value& other) const { 
-        return Value(num * other.num); 
-    }
-    Value operator/(const Value& other) const {
-        if (other.num == 0) 
+    int val;
+    Value(int v = 0) : val(v) {}
+    inline Value operator+(const Value &other) const { return Value(val + other.val); }
+    inline Value operator-(const Value &other) const { return Value(val - other.val); }
+    inline Value operator*(const Value &other) const { return Value(val * other.val); }
+    inline Value operator/(const Value &other) const
+    {
+        if (other.val == 0)
             throw runtime_error("Division by zero");
-        return Value(num / other.num);
+        return Value(val / other.val);
     }
+    friend class Interpreter;
 };
 
-class Interpreter {
+class Interpreter
+{
 public:
-    Value visit(TreeNode* tree) {
-        if (tree->nodetype == NodeType::number) {
-            return Value(tree->data);
+    Value visit(BaseNode *node)
+    {
+        if (node->getType() == NodeType::NUMBER)
+        {
+            NumNode *n = (NumNode *)node;
+            return Value(n->getValue());
         }
-        else if (tree->nodetype == NodeType::binop) {
-            Value left = visit(tree->left);
-            Value right = visit(tree->right);
-            
-            switch (tree->op.type) {
-                case TokenType::plus:  return left + right;
-                case TokenType::minus: return left - right;
-                case TokenType::mul:   return left * right;
-                case TokenType::div:   return left / right;
-                default: return Value(0);
+        else if (node->getType() == NodeType::BINOP)
+        {
+            BinOpNode *b = (BinOpNode *)node;
+            Value left = visit(b->getLeft());
+            Value right = visit(b->getRight());
+            switch (b->getOp().type)
+            {
+            case TokenType::PLUS:
+                return left + right;
+            case TokenType::MINUS:
+                return left - right;
+            case TokenType::MUL:
+                return left * right;
+            case TokenType::DIV:
+                return left / right;
+            default:
+                return Value(0);
             }
         }
         return Value(0);
     }
 };
 
-int main() {
-    string expr;
+int main()
+{
+    string s;
     cout << "Enter expression: ";
-    getline(cin, expr);
+    getline(cin, s);
 
-    Lexer lexer(expr);
+    Lexer lexer(s);
     Parser parser(lexer);
-    TreeNode* tree = parser.parse();
+    BaseNode *tree = parser.parse();
 
-    Interpreter calc;
-    try {
-        Value answer = calc.visit(tree);
-        cout << "Result: " << answer.num << endl;
-    } catch (exception& e) {
+    Interpreter interp;
+    try
+    {
+        Value result = interp.visit(tree);
+        cout << "Result = " << result.val << endl;
+    }
+    catch (exception &e)
+    {
         cerr << "Error: " << e.what() << endl;
     }
 
